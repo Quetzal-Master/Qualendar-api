53c40fc4e424349e945152de3506820567442056
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-06-10 11:53:33 +0200

    SSE instead of Websocket

--- server.js
+++ server.js
@@ -1,44 +1,57 @@
-// Require the framework and instantiate it
-const fastify = require("fastify")({ logger: true });
-fastify.register(require("@fastify/websocket"));
+// Require the framework
+const fastify = require('fastify')({ logger: true });
+const cors = require('@fastify/cors');  // Change this line
 
-let wsConnection = null;
+fastify.register(cors, {
+	origin: true, // Reflect the request origin
+	methods: ['GET', 'POST'], // The methods you're using
+	allowedHeaders: ['Content-Type'], // The headers you're using in your requests
+	credentials: true, // This allows cookies and credentials to be sent with requests
+});
 
-fastify.register(async function (fastify) {
-	fastify.get("/websocket-connect", { websocket: true }, (connection /* SocketStream */, req /* FastifyRequest */) => {
-		connection.socket.on("message", (message) => {
-			// message.toString() === 'hi from client'
-			connection.socket.send("hi from server");
-			wsConnection = connection;
-		});
+let sseConnection = null;
 
-		connection.socket.on("close", (error) => {
-			console.log(`WebSocket closed: ${error}`);
-		});
+fastify.get('/events', async function (req, reply) {
+	reply.raw.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	reply.raw.setHeader('Access-Control-Allow-Methods', 'GET');
+	reply.raw.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	reply.raw.setHeader('Access-Control-Allow-Credentials', 'true'); // Add this line
+	reply.raw.setHeader('Content-Type', 'text/event-stream');
+	reply.raw.setHeader('Cache-Control', 'no-cache');
+	reply.raw.setHeader('Connection', 'keep-alive');
+	reply.raw.flushHeaders();
+
+	req.raw.on('close', () => {
+		sseConnection = null;
+		console.log('SSE connection closed');
+		reply.raw.end();
 	});
+
+	sseConnection = reply.raw;
 });
 
-fastify.post("/webhook", (req, reply) => {
-	replyCode = 201;
-	if (wsConnection) {
+fastify.post('/webhook', (req, reply) => {
+	let replyCode = 201;
+
+	if (sseConnection) {
 		replyCode = 200;
-		// Send a message to the WebSocket client
-		wsConnection.socket.send("A new GET request has been received");
+		sseConnection.write(`data: {"message": "A new POST request has been received"}\n\n`);
 	}
-	reply.code(replyCode);
+	reply.code(replyCode).send("SIUUU");
 });
 
-fastify.get("/hello", (req, reply) => {
-	return reply.code(200).send({ hello: "world" });
+fastify.get('/hello', (req, reply) => {
+	return reply.code(200).send({ hello: 'world' });
 });
 
 // Run the server!
 const start = async () => {
 	try {
-		await fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
+		await fastify.listen({ port: process.env.PORT || 8000, host: '0.0.0.0' });
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
 	}
 };
+
 start();
