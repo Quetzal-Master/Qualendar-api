b19d5f6dd500ac05d491bbc0710315ffb488bdd0
Quentin HATTSTADT <qhattstadt@gmail.com>
2023-09-22 08:29:36 +0200

    Fix hearbeat server side

--- server.js
+++ server.js
@@ -17,39 +17,37 @@ let sseReply = null;
 fastify.get("/events", (request, reply) => {
 	console.log("SSE connection requested");
 
-	// Stocker la réponse 'reply' pour une utilisation ultérieure
 	sseReply = reply;
 
 	try {
-		// Envoi de heartbeats réguliers
+		sseReply.sse({ data: "start" });
+
 		const heartbeat = setInterval(() => {
-			console.log("Sending heartbeat");
-			sseReply.sse({ data: "heartbeat" });
+			if (sseReply != null) {
+				console.log("Sending heartbeat");
+				sseReply.sse({ data: "heartbeat" });
+			}
 		}, HEARTBEAT_INTERVAL);
 
-		// Écouter la fermeture de la connexion et arrêter les heartbeats
 		request.raw.on("close", () => {
 			console.log("SSE connection closed");
 			clearInterval(heartbeat);
-			sseReply = null; // Réinitialiser sseReply lorsque la connexion est fermée
+			sseReply = null;
 		});
 	} catch (error) {
 		console.error("Error in SSE route:", error);
 	}
 });
 
-fastify.post("/webhook", (req, reply) => {
+fastify.post("/webhook", () => {
 	let replyCode = 201;
 
 	if (sseReply) {
 		replyCode = 200;
 		sseReply.sse({
-			data: JSON.stringify({
-				message: "A new POST request has been received",
-			}),
+			data: "update",
 		});
 	}
-	reply.code(replyCode).send("SIUUU");
 });
 
 fastify.get("/hello", (req, reply) => {
